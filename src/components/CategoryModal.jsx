3cd38f3de52cd433e4e4306c90b4fad9dc9780a0
Гантулга Мөнхдэлгэр <gantulga@coremind.mn>
2024-03-04 18:27:13 +0800

    from Work

--- src/components/CategoryModal.jsx
+++ src/components/CategoryModal.jsx
@@ -1,6 +1,9 @@
 import { CloseModalIcon } from "@/components/Icons";
 import { AddCategoryModalButton } from "@/components/Buttons";
 import { CategoryIcons } from "@/dummyData";
+import IconPicker from "react-icon-picker";
+import { useState } from "react";
+
 import {
   WifiIcon,
   GeldIconWhite,
@@ -24,7 +27,17 @@ import {
   CateMicIcon,
 } from "./Icons/Icons";
 
+const icons = [
+  "fas fa-camera",
+  "fas fa-fish",
+  "fas fa-align-center",
+  "fas fa-align-justify",
+];
+
 export function CategoryModal() {
+  const [state, setState] = useState({
+    icon: "",
+  });
   return (
     <div>
       <div className="w-[494px] h-[68px] px-6 py-5 bg-white border-b border-slate-200 justify-between items-center inline-flex">
@@ -43,15 +56,17 @@ export function CategoryModal() {
         <div className="self-stretch h-[120px] flex-col justify-start items-start gap-8 flex">
           <div className="self-stretch justify-start items-start gap-3 inline-flex">
             <div className="justify-start items-start gap-3 flex">
-              <select className="select select-bordered rounded-lg flex-col justify-center items-center inline-flex w-[84px] h-12 p-4 bg-gray-50">
-                <option disabled selected></option>
-                {CategoryIcons.map((icon) => (
-                  <option>
-                    {/* {icon.icon} */}
-                    {/* <div className="w-6 h-6">{icon.icon}</div> */}
-                  </option>
-                ))}
-              </select>
+              <span className={state.icon}></span>
+              <IconPicker
+                icons={icons}
+                defaultValue="fas fa-camera"
+                onChange={(icon) => {
+                  setState({
+                    ...state,
+                    icon,
+                  });
+                }}
+              />
             </div>
             <select className="select select-bordered w-[350px] h-12 self-stretch bg-gray-50 rounded-lg border border-gray-300 justify-start items-center inline-flex">
               <option disabled selected>
